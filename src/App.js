96bad9db21a843d7bd09f2946ae2a8577bcd4e76
kedaroo <krb1202@gmail.com>
2022-03-11 00:02:20 +0530

    feat: more emojis!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const emojiDictionary ={
+  "😂": "laughing",
+  "😉": "winking",
+  "😎": "cool",
+  "😍": "lovely",
+  "😏": "smirking",
+  "😒": "unamused",
+  "🤑": "greedy",
+}
+
+const emojis = Object.keys(emojiDictionary);
+
 function App() {
+
+  const [meaning, setMeaning] = useState("meaning will appear here");
+
+  function emojiChangeInputHandler(event) {
+    let emojiInput = event.target.value;
+    let meaning = emojiDictionary[emojiInput];
+    if (meaning === undefined) {
+      meaning = "we don't have the meaning to this emoji!"
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(item) {
+    let meaning = emojiDictionary[item];
+    if (meaning === undefined) {
+      meaning = "we don't have the meaning to this emoji!"
+    }
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Emoji Interpreter!</h1>
+      <input
+        className="input-emoji" 
+        onChange={emojiChangeInputHandler} 
+        placeholder="enter your emoji here"
+      ></input>
+      <div className="div-meaning">{meaning}</div>
+      <ul className="list non-bullet">
+        {
+          emojis.map(item => (
+            <li
+              className="list-inline list-emoji" 
+              onClick={() => emojiClickHandler(item)}
+            >{item}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
